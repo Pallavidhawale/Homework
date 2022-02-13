e52094ba2f433d3e7a92178887db0fb7cdd344e1
Pallavidhawale <91894601+Pallavidhawale@users.noreply.github.com>
2022-02-13 08:49:27 +0530

    Add files via upload

---
+++ CreatingHtmlPageWebGuiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatingHtmlPageWebGuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatingHtmlPageWebGuiApplication.class, args);
+	}
+
+}
